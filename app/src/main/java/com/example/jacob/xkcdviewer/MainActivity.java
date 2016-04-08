dc4fe9fd9f9829efa4f8628041fe05bbdf89c5f0
Jacob Malachowski <jmalachowski623@gmail.com>
2016-04-07 22:16:16 -0500

    Finished App

--- app/src/main/java/com/example/jacob/xkcdviewer/MainActivity.java
+++ app/src/main/java/com/example/jacob/xkcdviewer/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.jacob.xkcdviewer;
 
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
